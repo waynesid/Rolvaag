aed874b2f5f01d67c015dfb516fc7dd0b7e10169
waynesid <sidneywaynex@outlook.com>
2023-11-27 16:38:38 +0300

    neuralnet.py architecture fix

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,4 @@
-import torch.nn as nn 
+import torch.nn as nn
 class NeuralNetworkClassificationModel(nn.Module):
     def __init__(self,input_dim,output_dim):
         super(NeuralNetworkClassificationModel,self).__init__()
@@ -6,10 +6,11 @@ class NeuralNetworkClassificationModel(nn.Module):
         self.hidden_layer1  = nn.Linear(32,64)
         self.output_layer   = nn.Linear(64,output_dim)
         self.relu = nn.ReLU()
-    
+        self.softmax = nn.Softmax(dim=1)  # Add this line
     
     def forward(self,x):
         out =  self.relu(self.input_layer(x))
         out =  self.relu(self.hidden_layer1(out))
         out =  self.output_layer(out)
+        out =  self.softmax(out)  # Add this line
         return out
